43be7b7f6355567be866fdd2d9ea9c18e3d83c94
Mike Rogers <me@mikerogers.io>
2019-02-28 21:50:19 +0000

    https only for connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://react-js-rails-sample.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://react-js-rails-sample.herokuapp.com' ]
+  config.action_cable.allowed_request_origins = [ 'https://react-js-rails-sample.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
